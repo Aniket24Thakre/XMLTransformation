8a32f4b31c9164d10fa840e59aa6c1d83391b478
gajanankulkarni18 <79788189+gajanankulkarni18@users.noreply.github.com>
2021-04-12 17:09:17 +0530

    Added TODO - please check the feasibility and modify it accordingly

--- src/main/java/XmlTransformation.java
+++ src/main/java/XmlTransformation.java
@@ -19,18 +19,29 @@ public class XmlTransformation {
         String filesPath = null;
 
         System.out.println("Enter InputXsltFile Name");
+        //TODO - 1
+         //Replace and try
+         //Instead of new Scanner(new InputStreamReader(System.in)) use new Scanner(System.in)
         Scanner inputXsltfile = new Scanner(new InputStreamReader(System.in));
         String xsltFileName = inputXsltfile.nextLine();
 
         System.out.println("Enter InputXMLFile Name");
+        //TODO - 2
+        //Replace and try
+        //Instead of new Scanner(new InputStreamReader(System.in)) use new Scanner(System.in)
         Scanner inputXMLFilename = new Scanner(new InputStreamReader(System.in));
         String xmlFileName = inputXMLFilename.nextLine();
 
         System.out.println("Enter OutputXMLFile Name");
+        //TODO - 3
+        //Replace and try
+        //Instead of new Scanner(new InputStreamReader(System.in)) use new Scanner(System.in)
         Scanner outputXMLFilename = new Scanner(new InputStreamReader(System.in));
         String outXmlFileName = outputXMLFilename.nextLine();
 
         Properties getProperties = new Properties();
+        //TODO - 4
+        //Please check feasibility of merging 2 try blocks into one
         try(InputStream inputStream = new FileInputStream(PROPERTY_FILE_LOCATION)){
             if(inputStream !=null){
                 getProperties.load(inputStream);
